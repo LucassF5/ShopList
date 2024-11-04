b4859961dc267af9e2129885474a21a6f142dc67
LucasF <franco100lucas@gmail.com>
2024-11-04 10:19:13 -0300

    Fix: Corrigindo mensagem de erro

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -5,7 +5,7 @@ class Produto < ApplicationRecord
   validates :nome, :quantidade, presence: true
   validates :nome, length: { minimum: 3, maximum: 50 }, format: { with: /\A[a-zA-Z]+\z/, message: "Apenas letras são permitidas" }
   # REGEX: \A -> Inicio da string, [a-zA-Z] -> Letras de A a Z, + -> 1 ou mais, \z -> Fim da string
-  validates :quantidade, numericality: { only_integer: true, greater_than: 0 }
+  validates :quantidade, numericality: { only_integer: true, greater_than: 0 , message: "Apenas números inteiros são permitidos" }
 
   private
 
